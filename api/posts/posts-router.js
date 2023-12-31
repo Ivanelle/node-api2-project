ef5c15681c67e992e52e6ad8463e1de92b7305c2
Ivanelle Hunt <ivanellep@gmail.com>
2023-12-31 15:41:08 -0800

    7 passing

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -18,12 +18,47 @@ router.get('/', (req, res) => {
         })
 })
 
-router.get('/', (req, res) => {
-
+router.get('/:id', async (req, res) => {
+    try {
+       const post = await Post.findById(req.params.id) 
+       if (!post) {
+         res.status(404).json({
+            message: "The post with the specified ID des not exist",
+        })
+       } else {
+        res.json(post)
+       }
+    } catch (err) {
+        res.status(500).json({
+            message: "The post information could not be retrieved",
+            err: err.message,
+            stack: err.stack,
+        })
+    }
 })
 
 router.post('/', (req, res) => {
-
+    const { title, contents } = req.body;
+    if(!title || !contents) {
+        res.status(400).json({
+            message: "Please provide title and cotents for the post",
+        })
+    } else {
+        Post.insert({ title, contents })
+            .then(({ id }) => {
+                return Post.findById(id)
+            })
+            .then(post => {
+                res.status(201).json(post)
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: "There was an error while saving the post to the database",
+                    err: err.message,
+                    stack: err.stack,
+                })
+            })
+    }
 })
 
 router.delete('/', (req, res) => {

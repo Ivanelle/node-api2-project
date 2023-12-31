8a133d602287016eb6e87065f909105edb6fdd35
Ivanelle Hunt <ivanellep@gmail.com>
2023-12-31 12:42:06 -0800

    three tests passing

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,6 +1,43 @@
 // implement your posts router here
 const express = require('express');
+const Post = require('./posts-model');
 
 const router = express.Router();
+
+router.get('/', (req, res) => {
+    Post.find()
+        .then(found => {
+            res.json(found)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "The posts information could not be retrieved",
+                err: err.message,
+                stack: err.stack
+            })
+        })
+})
+
+router.get('/', (req, res) => {
+
+})
+
+router.post('/', (req, res) => {
+
+})
+
+router.delete('/', (req, res) => {
+
+})
+
+router.put('/', (req, res) => {
+
+})
+
+router.get('/', (req, res) => {
+
+})
+
+
 
 module.exports = router;
